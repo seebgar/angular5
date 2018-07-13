9559766e4f5de2831d2bcc8adcbfd07048bddb67
Sebastian Garcia <js.garcial1@uniandes.edu.co>
2018-07-12 22:50:59 -0500

    servevr

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/my-app'));
 
 app.get('/*', function(req,res) {
     
     // /dist/my-app/index.html
-res.sendFile(path.join(__dirname+'/src/app.component.html'));
+res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
