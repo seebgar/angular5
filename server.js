79161bac447ddc4e21e524a76af474aa560b2c2f
Sebastian Garcia <js.garcial1@uniandes.edu.co>
2018-07-12 22:44:23 -0500

    index

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+    // /dist/my-app/index.html
+res.sendFile(path.join(__dirname+'/src/app.component.html'));
 });
 
 // Start the app by listening on the default Heroku port
